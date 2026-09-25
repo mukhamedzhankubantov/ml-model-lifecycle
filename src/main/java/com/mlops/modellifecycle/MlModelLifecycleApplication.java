ede51d4a6d4c65cf3c311dd2976e4dc7009965ce
Kubantov Muhamedzhan <mukhamedzhan.kubantov@narxoz.kz>
2026-09-25 17:11:48 +0500

    feat: initial project structure with Spring Boot

---
+++ src/main/java/com/mlops/modellifecycle/MlModelLifecycleApplication.java
@@ -0,0 +1,13 @@
+package com.mlops.modellifecycle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MlModelLifecycleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MlModelLifecycleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mlops/modellifecycle/MlModelLifecycleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mlops.modellifecycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MlModelLifecycleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
